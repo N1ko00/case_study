f9a1556e52760e6f9518e1b045cdae622ce14cac
deneso1107 <sk1529rt@icloud.com>
2026-04-17 14:47:23 +0900

    Title遷移を追加
    ついでにフェードもつけときました

---
+++ Assets/Script/Scene/Title.cs
@@ -0,0 +1,33 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class Title : MonoBehaviour
+{
+    InputSystem_Actions inputAction;
+
+    //めんどくなったのでインスタンス参照
+    [SerializeField] SceneLoader sceneLoader;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        inputAction.Scene.Move.performed += ctx => sceneLoader.LoadScene(SceneLoader.SceneName.MainScene);
+
+    }
+
+    void Awake()
+    {
+        inputAction = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        inputAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputAction.Disable();
+    }
+}
---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneLoader : MonoBehaviour
+{
+    public Image fadeImage; // フェード用のイメージ
+    public float  fadeDuration = 0.5f; // フェードの時間
+    // シーン名の列挙型
+    public enum SceneName
+    {
+        TitleScene,
+        MainScene,
+        GameOverScene,
+        GameClearScene,
+        ResultScene
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoadScene(SceneName name)
+    {
+        StartCoroutine(LoadSceneCoroutine(name));
+    }
+
+    IEnumerator LoadSceneCoroutine(SceneName name)
+    {
+        yield return FadeOut();
+        SceneManager.LoadScene(name.ToString());
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float a = t / fadeDuration;
+            fadeImage.color = new Color(0, 0, 0, a);
+            yield return null;
+        }
+    }
+}
