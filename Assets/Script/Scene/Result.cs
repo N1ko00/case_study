046267d59249633a2782c713cc11a8de7f5e83a7
N1ko00 <ohs30279@ohs.hal.ac.jp>
2026-05-29 10:33:43 +0900

    シーン遷移関連修正
    TitleSceneの誤字修正
    リザルトシーンのタイトルへ戻るの追加

--- Assets/Script/Scene/Result.cs
+++ Assets/Script/Scene/Result.cs
@@ -1,16 +1,43 @@
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class Result : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    InputSystem_Actions inputAction;
+
+    [SerializeField] SceneLoader sceneLoader;
+
+    [Header("ボタン参照")]
+    [SerializeField] private Button ReturnTitle;
+
+    void OnEnable()
     {
-        
+        if (ReturnTitle != null)
+        {
+            ReturnTitle.onClick.AddListener(OnReturnTitleClicked);
+        }
+    }
+    
+    void OnDisable()
+    {
+        if (ReturnTitle != null)
+        {
+            ReturnTitle.onClick.RemoveListener(OnReturnTitleClicked);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnquitClicked()
     {
-        
+        sceneLoader.LoadScene(SceneLoader.SceneName.TitleScene);
+    }
+
+    private void OnReturnTitleClicked()
+    {
+        Debug.Log("[Result] 終了");
+        sceneLoader.LoadScene(SceneLoader.SceneName.TitleScene);
+
     }
 }
